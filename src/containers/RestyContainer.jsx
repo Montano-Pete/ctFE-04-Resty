4cddf0f494ecf31a15131ef6fa2a7b6680c3c2e2
Peter <pete225848@gmail.com>
2021-11-08 08:12:31 -0800

    super simple skeleton for container with expected state

---
+++ src/containers/RestyContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class RestyContainer extends Component {
+  state = {
+    url: '',
+    method: '',
+    body: '',
+    history: [],
+  };
+
+  render() {
+    return <></>;
+  }
+}
